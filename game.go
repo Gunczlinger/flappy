562cfcc258396ff7808e5b5abfbf3d58d29a18f9
Gunczlinger <22249348+Gunczlinger@users.noreply.github.com>
2019-05-18 14:58:14 -0400

    First submission
    Changed control flow to add start screen; added timer and high score

--- game.go
+++ game.go
@@ -4,10 +4,16 @@
 
 // +build darwin linux
 
+// All text rendering is heavily referenced if not copied directly from "https://github.com/antoine-richard/gomobile-text", and all copyright to such code belongs to said party
+
+
 package main
 
 import (
+	"fmt" // Trying to use to print text--we'll see if it works (new)
 	"image"
+	"image/color" //new
+
 	"log"
 	"math"
 	"math/rand"
@@ -18,17 +24,25 @@ import (
 	"golang.org/x/mobile/exp/f32"
 	"golang.org/x/mobile/exp/sprite"
 	"golang.org/x/mobile/exp/sprite/clock"
+
+//All remaining import statements copied from "https://github.com/antoine-richard/gomobile-text"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/geom"
+	"golang.org/x/mobile/gl"
 )
 
 const (
 /*
 Modified physical constants:
 -jump velocity (jumps higher) *ended up unbalancing the game--DEPRECATED*
--flap velocity (twice yhe airtime)
+-flap velocity (twice the airtime)
 -game speed (starts faster, accelerates faster)
--climbing grace (less likely to trip over minor height change)
+-climbing grace (less likely to trip over minor height change
+-dead time before reset (increased by a second)
 
-*Iportant to note that it appears 'y' is reversed--will try to modify touch sensitivity based on this assumption*
+*Important to note that it appears 'y' is reversed--will try to modify touch sensitivity based on this assumption*
 */
 
 	tileWidth, tileHeight = 16, 16 // width and height of each tile
@@ -43,7 +57,7 @@ Modified physical constants:
 	flapV       = -3  // flap velocity
 
 	deadScrollA         = -0.01 // scroll deceleration after the gopher dies
-	deadTimeBeforeReset = 240   // how long to wait before restarting the game
+	deadTimeBeforeReset = 300   // how long to wait before restarting the game
 
 	groundChangeProb = 5 // 1/probability of ground height change
 	groundWobbleProb = 3 // 1/probability of minor ground height change
@@ -55,6 +69,11 @@ Modified physical constants:
 )
 
 type Game struct {
+	start    	bool       		// display launch screen? <-- NEW
+	startTime	clock.Time 		// to find playing time <-- NEW
+	bestRound   clock.Time 		// temporary high score <-- NEW
+	bestTime    string          //cut down on processing<-- NEW
+	displayTime	clock.Time 		// playing time to display <-- NEW
 	gopher struct {
 		y        float32    // y-offset
 		v        float32    // velocity
@@ -70,16 +89,21 @@ type Game struct {
 	groundY   [tilesX + 3]float32 // ground y-offsets
 	groundTex [tilesX + 3]int     // ground texture
 	lastCalc  clock.Time          // when we last calculated a frame
+	font       *truetype.Font     // font selector from "https://github.com/antoine-richard/gomobile-text"
 }
 
 func NewGame() *Game {
 	var g Game
 	g.reset()
+	g.bestRound = 0
 	return &g
 }
 
 func (g *Game) reset() {
-	g.gopher.y = 0
+	g.start = false
+	g.bestTime = fmt.Sprintf("Best: %.2d:%.2d.%d", g.bestRound/3600, (g.bestRound%3600)/60, g.bestRound%60)
+	g.startTime = 0	// timer starts, visible, at 0 <-- NEW
+	g.gopher.y = -8 // Gopher needs to start more offscreen after start screen added <-- MODIFIED
 	g.gopher.v = 0
 	g.scroll.x = 0
 	g.scroll.v = initScrollV
@@ -91,6 +115,13 @@ func (g *Game) reset() {
 	g.gopher.flapped = false
 	g.gopher.dead = false
 	g.gopher.deadTime = 0
+
+//Remainder of function from "https://github.com/antoine-richard/gomobile-text"
+	var err error
+	g.font, err = LoadCustomFont()
+	if err != nil {
+		log.Fatalf("error parsing font: %v", err)
+	}
 }
 
 func (g *Game) Scene(eng sprite.Engine) *sprite.Node {
@@ -165,7 +196,7 @@ func frame(t, d clock.Time, frames ...int) int {
 func animateDeadGopher(a *f32.Affine, t clock.Time) {
 	dt := float32(t)
 	a.Scale(a, 1+dt/20, 1+dt/20)
-	a.Translate(a, 0.5, 0.5)
+	a.Translate(a, 0.1, 0.4)
 	a.Rotate(a, dt/math.Pi/-8)
 	a.Translate(a, -0.5, -0.5)
 }
@@ -234,15 +265,24 @@ func (g *Game) Press(down bool) {
 	}
 
 	if down {
-		switch {
-		case g.gopher.atRest:
-			// Gopher may jump from the ground.
-			g.gopher.v = jumpV
-		case !g.gopher.flapped:
-			// Gopher may flap once in mid-air.
-			g.gopher.flapped = true
-			g.gopher.v = flapV
-		}
+
+		if g.start {	//trying this control to have tap to start
+
+			switch {
+			case g.gopher.atRest:
+				// Gopher may jump from the ground.
+				g.gopher.v = jumpV
+			case !g.gopher.flapped:
+				// Gopher may flap once in mid-air.
+				g.gopher.flapped = true
+				g.gopher.v = flapV
+			}
+
+		} else {
+			g.start = true
+			g.startTime = g.lastCalc
+			}
+
 	} else {
 		// Stop gopher rising on button release.
 		if g.gopher.v < 0 {
@@ -254,12 +294,24 @@ func (g *Game) Press(down bool) {
 func (g *Game) Update(now clock.Time) {
 	if g.gopher.dead && now-g.gopher.deadTime > deadTimeBeforeReset {
 		// Restart if the gopher has been dead for a while.
+		if (g.displayTime > g.bestRound){
+			g.bestRound = g.displayTime
+		}
 		g.reset()
+	}
+
+	// Next 4 lines update the timer
+	if g.gopher.dead || !g.start{
+		g.displayTime = (g.gopher.deadTime-g.startTime)
+	} else {
+		g.displayTime = now-g.startTime
 	}
 
 	// Compute game states up to now.
 	for ; g.lastCalc < now; g.lastCalc++ {
-		g.calcFrame()
+		if g.start{	// Traps game until game is started: MUST BE HERE in order to interrupt control flow without hanging app (could be in calcFrame(), but that adds unecessary function call) <-- NEW
+			g.calcFrame()
+		}
 	}
 }
 
@@ -363,4 +415,75 @@ func (g *Game) clampToGround() {
 		g.gopher.atRest = true
 		g.gopher.flapped = false
 	}
+}
+
+
+//The following functions were adapted from "https://github.com/antoine-richard/gomobile-text"
+func (g *Game) Render(sz size.Event, glctx gl.Context, images *glutil.Images) {
+
+	if !g.start{
+			startText1 := &TextSprite{
+				placeholder:     "TOUCH ANYWHERE",
+				text:            "TOUCH ANYWHERE",
+				font:            g.font,
+				widthPx:         sz.WidthPx,
+				heightPx:        (sz.HeightPx / 4),
+				textColor:       image.White,
+				backgroundColor: image.NewUniform(color.RGBA{0x4B, 0x00, 0x82, 0xFF}),
+				fontSize:        116,
+				xPt:             0,
+				yPt:             PxToPt(sz, (3 * sz.HeightPx / 10)),
+				align:           Center,
+			}
+			startText2 := &TextSprite{
+				placeholder:     "TO START!",
+				text:            "TO START!",
+				font:            g.font,
+				widthPx:         sz.WidthPx,
+				heightPx:        (sz.HeightPx / 6),
+				textColor:       image.White,
+				backgroundColor: image.NewUniform(color.RGBA{0x4B, 0x00, 0x82, 0xFF}),
+				fontSize:        116,
+				xPt:             0,
+				yPt:             PxToPt(sz, (sz.HeightPx / 2)),//-(sz.HeightPx / 12)),
+				align:           Center,
+			}
+			startText1.Render(sz)
+			startText2.Render(sz)
+	}
+
+	bTimer := &TextSprite{
+		placeholder:     "TIMER", // Changed value
+		text:            fmt.Sprintf("%s",g.bestTime), //Completely my own
+		font:            g.font,
+		widthPx:         sz.WidthPx,
+		heightPx:        (sz.HeightPx / 12), //reduced by 11/12
+		textColor:       image.NewUniform(color.RGBA{0xDA, 0xA5, 0x20, 0xFF}), // reversed background and text colors
+		backgroundColor: image.White, // reversed background and text colors
+		fontSize:        116, //changed font size from 96
+		xPt:             PxToPt(sz, sz.WidthPx/4),//-PxToPt(sz, (sz.WidthPx / 2)), // adapted PxToPt() function for own use
+		yPt:             PxToPt(sz, (sz.HeightPx / 24)), // adapted PxToPt() function for own use, lower by 1/24 the screen
+		align:           Left, // added field
+	}
+	bTimer.Render(sz)
+
+	gTimer := &TextSprite{
+		placeholder:     "TIMER", // Changed value
+		text:            fmt.Sprintf("%.2d:%.2d.%d", g.displayTime/3600, (g.displayTime%3600)/60, g.displayTime%60), //Completely my own
+		font:            g.font,
+		widthPx:         sz.WidthPx / 2, //reduced by half
+		heightPx:        (sz.HeightPx / 12), //reduced by 11/12
+		textColor:       image.NewUniform(color.RGBA{0x35, 0x67, 0x99, 0xFF}), // reversed background and text colors
+		backgroundColor: image.White, // reversed background and text colors
+		fontSize:        116, //changed font size from 96
+		xPt:             PxToPt(sz, (sz.WidthPx / 2)), // adapted PxToPt() function for own use
+		yPt:             PxToPt(sz, (3*sz.HeightPx / 24)), // adapted PxToPt() function for own use
+		align:           Left, // added field
+	}
+	gTimer.Render(sz)
+}
+
+// PxToPt convert a size from pixels to points (based on screen PixelsPerPt)
+func PxToPt(sz size.Event, sizePx int) geom.Pt {
+	return geom.Pt(float32(sizePx) / sz.PixelsPerPt)
 }
